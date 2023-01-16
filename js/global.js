701da8d265a8cfe0bd27665ac31ddc98fb49fc61
That_One_Nerd <97963658+GilberK25@users.noreply.github.com>
2023-01-16 14:27:50 -0500

    made some more progress on the design
    im gonna remake it to resemble wikipedia more though

--- js/global.js
+++ js/global.js
@@ -3,11 +3,13 @@ const internet = new XMLHttpRequest();
 var htmlStr = document.documentElement.innerHTML;
 
 // Insert site parts
-insertHeader();
-insertFooter();
+//insertHeader();
+insertContents();
+//insertFooter();
 
 // Update page
 document.documentElement.innerHTML = htmlStr;
+alert("test");
 
 // ----- Begin function declarations
 
@@ -23,6 +25,38 @@ function insertHeader()
         const header = internet.responseText;
         while (htmlStr.includes(headerReplace)) htmlStr = htmlStr.replace(headerReplace, header);
     }
+}
+function insertContents()
+{
+    const tableReplace = "<!--TABLE OF CONTENTS-->";
+
+    var table = document.createElement("div");
+    table.setAttribute("id", "table-of-contents");
+
+    var title = document.createElement("h2");
+    title.textContent = "Table of Contents";
+
+    table.appendChild(title);
+
+    var list = document.createElement("ul");
+
+    var titleObjects = document.getElementsByClassName("para-title");
+    for (var i = 0; i < titleObjects.length; i++)
+    {
+        var appendLink = document.createElement("a");
+        appendLink.setAttribute("class", "nounderline");
+        appendLink.setAttribute("href", "#" + titleObjects[i].id);
+        appendLink.textContent = titleObjects[i].textContent;
+
+        var append = document.createElement("li");
+        append.appendChild(appendLink);
+
+        list.appendChild(append);
+    }
+
+    table.appendChild(list);
+
+    while (htmlStr.includes(tableReplace)) htmlStr = htmlStr.replace(tableReplace, table.outerHTML);
 }
 function insertFooter()
 {
